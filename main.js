86643b5a45f2325de873abea3967d65818bbd5ce
Kor.Alina <anrezn@mail.ru>
2025-03-18 20:39:09 +0300

    js-button

--- main.js
+++ main.js
@@ -18,3 +18,32 @@ function updateBullets() {
 slidesContainer.addEventListener('scroll', updateBullets);
 
 // ПОКАЗ ЭЕЛЕМЕНТОВ
+let button = document.querySelector('.brend-list__show-button');
+let btntext = document.querySelector('.show-btn');
+let rotate = document.querySelector('.arrows-top');
+let brends = document.querySelector('.brend-list__logo')
+button.onclick = function () {
+    if (button.classList.contains('close-btn')) {
+
+        brends.classList.remove('closed-brend-list')
+        button.classList.remove('close-btn')
+        rotate.classList.remove('non-rot')
+
+        brends.classList.add('open-brend-list')
+        button.classList.add('open-btn')
+        rotate.classList.add('rot')
+
+        btntext.value = 'Скрыть'
+} else {
+    brends.classList.remove('open-brend-list')
+    button.classList.remove('open-btn')
+    rotate.classList.remove('rot')
+
+    brends.classList.add('closed-brend-list')
+    button.classList.add('close-btn')
+    rotate.classList.add('non-rot')
+    
+    btntext.value = 'Показать все'
+}
+
+}
